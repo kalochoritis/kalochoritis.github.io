89a1aaeb927dc7200516d4d3adc2b50830616421
kalochoritis <48739003+kalochoritis@users.noreply.github.com>
2019-03-20 16:24:12 +0000

    Update init.js

--- init.js
+++ init.js
@@ -10,36 +10,36 @@
 		reset: 'full',
 		breakpoints: {
 			global: {
-				href: 'css/style.css',
+				href: 'https://kalochoritis.github.io/style.css',
 				containers: 1400,
 				grid: { gutters: ['2em', 0] }
 			},
 			xlarge: {
 				media: '(max-width: 1680px)',
-				href: 'css/style-xlarge.css',
+				href: 'https://kalochoritis.github.io/style-xlarge.css',
 				containers: 1200
 			},
 			large: {
 				media: '(max-width: 1280px)',
-				href: 'css/style-large.css',
+				href: 'https://kalochoritis.github.io/style-large.css',
 				containers: 960,
 				grid: { gutters: ['1.5em', 0] },
 				viewport: { scalable: false }
 			},
 			medium: {
 				media: '(max-width: 980px)',
-				href: 'css/style-medium.css',
+				href: 'https://kalochoritis.github.io/style-medium.css',
 				containers: '90%!'
 			},
 			small: {
 				media: '(max-width: 736px)',
-				href: 'css/style-small.css',
+				href: 'https://kalochoritis.github.io/style-small.css',
 				containers: '90%!',
 				grid: { gutters: ['1.25em', 0] }
 			},
 			xsmall: {
 				media: '(max-width: 480px)',
-				href: 'css/style-xsmall.css'
+				href: 'https://kalochoritis.github.io/style-xsmall.css'
 			}
 		},
 		plugins: {
